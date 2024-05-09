7f6f43f6cf263e93d4edb66c172a444e39cf12fa
mohammadfaiz2722 <133015621+mohammadfaiz2722@users.noreply.github.com>
2024-05-09 16:59:01 +0530

    Updated

--- src/app/about/PersonalInfo.js
+++ src/app/about/PersonalInfo.js
@@ -16,28 +16,26 @@ const PersonalInfo = () => {
      
   
       <div className='about-box text custom'>
-        {/* <div className="move"> */}
-
-        <div className="head" id='head1'>Personal Info</div>
-        <div className="details" style={{marginTop:'-11%'}}>My name is Mohammad Faiz, and I was born in Faiz U.P on 27th February 2003. I am passionate about coding and gaming, spending my time exploring new programming concepts and enjoying immersive gaming experiences. These hobbies fuel my curiosity and drive for learning in the ever-evolving tech landscape.
-
+        <div className="myphoto"></div>
+        <div className="about-myself">I'm Mohammad Faiz, born and raised in Faizabad, Uttar Pradesh. (Interesting fact: Faizabad is also known as Ayodhya!)  While I may come across as a bit reserved at first – introverts unite! – I love a good laugh once I get to know someone. When I'm not coding or strategizing my next game plan (becoming a MERN stack developer and software engineer is the dream!), I might be curled up with a book or exploring hidden corners of the internet. My personality is a blend of seriousness and humor – it all depends on how comfortable I feel. But one thing's for sure, I'm always up for learning and growing, and I'm excited to see what the future holds! <br/>
+      <strong>  "My faith as a proud Muslim motivates me to be a kind and helpful person, values that I carry into all aspects of my life, including coding and teamwork."
+"In my free time, I might be found at the mosque with friends or reading about Islamic history and philosophy. But you can also find me conquering virtual quests in BGMI!"</strong>
 </div>
-        <div className="underline"></div>
       </div>
-        {/* </div> */}
-
-      {/* Second About Box */}
-      <div className='about-box text custom' style={{marginTop:"-6%"}} >
-        {/* <div className="underline"></div> */}
-        <div className="head " id='head2'  style={{marginTop:'-16%'}}>Educational Info</div>
-        <div className="details second-last-box" style={{marginTop:'-12%'}}>I completed my schooling at Avadh International (10+2) and am currently pursuing BCA at Jhunjhunwala PG College. My programming journey has equipped me with a diverse set of languages including C, C++, Python, Java, JavaScript, and the MERN stack. These experiences have not only enriched my technical skills but also fueled my passion for problem-solving and innovation in the digital realm.</div>
-        <div className="underline"></div>
+      <div className="heading text-center" id='About-me'>
+        My Education
       </div>
-      <div className='about-box text custom' id='last-box' style={{marginTop:"-3%"}} >
-        <div className="head" id='head3'style={{marginTop:'-20%'}}>Work History</div>
-        <div className="details last" style={{marginTop:'-16%'}} id='last-para'>In addition to my academic pursuits, I have been actively involved in coding for approximately three years, honing my skills and delving into various programming languages and technologies. This journey has not only deepened my understanding of software development but has also allowed me to work on diverse projects, from web development to software applications. My experiences have taught me the importance of continuous learning, problem-solving, and collaboration within teams. I am eager to leverage my coding expertise and contribute meaningfully to innovative tech projects that drive positive change and user experiences.</div>
+      <div className='about-box text custom'>
+        <div className="myeduc"></div>
+        <div className="about-myself">My educational journey began at New Wave Academy, where I nurtured my curiosity from nursery all the way to class 8. In ninth grade, I transitioned to Avadh International School, where I broadened my horizons until graduation. Now, I'm immersed in the third year of my BCA program at Jhunjhunwala PG College. Programming languages ignite a passion in me, and I'm constantly seeking out new ones to master. This journey is fueled by a dream of becoming a full-stack web developer and a skilled software engineer. I believe education is a lifelong pursuit, and I'm excited to see where this path of learning takes me next!</div>
       </div>
-
+      <div className="heading text-center" id='About-me'>
+        My Hobbies
+      </div>
+      <div className='about-box text custom'>
+        <div className="myhobbies"></div>
+        <div className="about-myself">My hobbies are a whirlwind of mental challenges and physical activity! When I'm not busy coding and building awesome programs or games (think creating my own mini-world!), I'm unwinding with a thrilling adventure in BGMI or strategizing the next big attack in Clash of Clans. But sometimes, the call of competition is too strong to resist. That's when you'll find me on the field, playing cricket or football – the camaraderie with teammates and the adrenaline rush of the game are simply unbeatable. To keep my mind sharp and my body energized for all these adventures, I'm a devoted gym rat. It's the perfect way to stay focused and feeling fantastic, whether I'm conquering a coding challenge, dominating a virtual world, or crushing it on the sports field!</div>
+      </div>
     </>
   );
 }
